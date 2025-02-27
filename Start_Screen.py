85b27030ac703b8414ab7980ad75ac1a526187eb
25cchappell <25cchappell@students.bhasd.org>
2025-02-27 12:45:28 -0500

    Added a init, set background, and start game function.

--- Start_Screen.py
+++ Start_Screen.py
@@ -1,2 +1,10 @@
 class StartScreen:
+
+    def __init__(self, backgroud):
+        self.background = background
     
+    def setBackground(self, backgroud):
+        pass
+
+    def startGame(self):
+        pass    
