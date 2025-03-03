d958a236a2de81782d979e9a63839c234f7b3bb6
25cshook <25cshook@students.bhasd.org>
2025-03-03 12:43:48 -0500

    Start SS

---
+++ Player.py
@@ -0,0 +1,17 @@
+# Player file
+
+# Tasks for cshook - ss_up, ss_down, ss_left, ss_right, ss_interact, ss_attack, up, down, left, right
+
+    #update with Rowan's code 
+
+        #self.ss_up = ss_up
+        #self.ss_down = ss_down
+        #self.ss_left = ss_left
+        #self.ss_right = ss_right
+        #self.ss_interact = ss_interact
+        #self.ss_attack = ss_attack
+
+        #actions
+        def up()
+            
+    
