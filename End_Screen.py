e9c8fd13d850d13f8b4252c728990209509b61c7
mr-chris-reed <chrree@bhasd.org>
2025-03-04 05:15:49 -0500

    added notes on files, added mr_reed_testing.py

--- End_Screen.py
+++ End_Screen.py
@@ -2,6 +2,14 @@ import pygame
 from Asset_Reader import Asset_Reader
 
 class End_Screen:
+    ###
+    # NOTES: You might consider blitting the runtime, leaderboard, gameOverMessage
+    # onto the backgroundGraphic before returning the end screen. We could make a
+    # leaderboard class, which is responsible for holding an array of top scores.
+    # The leaderboard could hold its data in a list of lists or dictionary.  We
+    # would want it to hold position, player's initials, and time to complete.
+    # We would also want a message to tell players how to get back to the start.
+    ###
     def __init__(
             self,
             x,
