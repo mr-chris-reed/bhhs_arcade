53997f5b1b5c517a88a2af360f2b620ea0aac5ab
CJames18 <25cjames@students.bhasd.org>
2025-03-13 13:12:49 -0400

    updated boundaries and trigger boxes

--- Background.py
+++ Background.py
@@ -1,26 +1,59 @@
 from Asset_Reader import Asset_Reader
 
 class Background:
-    def __init__(self, background, num_images, x, y, scale_factor):
-        ###
-        # NOTES: You might consider creating instance variables that define the
-        # movable space - the area where characters can move around - boundaries for background.
-        # One way to do this is to create instance variables of x_low, x_high, y_low, y_high. These instance variables
-        # can be used by the player and enimies to control the character not moving off of the screen.
-        # We need to discuss how we want to control background flow. Will it be part of this class,
-        # like you have below with the methods moveToNext and moveToPrev, or will this be controlled
-        # in the main loop?  Along with this challenge, we also need to think about how the character
-        # transitions to the next background.  Does it happen automatically when minions/boss is
-        # destroyed or is there a door?
-        ###
-        main
+    def __init__(self, background, num_images, x, y, scale_factor, origin_x, origin_y, width, height):
         self.background_list = Asset_Reader(background, num_images, scale_factor).get_asset_list()  # Load the backgrounds
         self.x = x
         self.y = y
-        self.origin_x = origin_x
-        self.origin_y = origin_y
-        self.width = width
-        self.height = height
+        self.boundary_x = boundary_x
+        self.boundary_y = boundary_y
+        self.boundary_width = boundary_width
+        self.boundary_height = boundary_height
+        self.next_x = next_x
+        self.next_y = next_y
+        self.next_width = next_width
+        self.next_height = next_height
+        self.prev_x = prev_x
+        self.prev_y = prev_y
+        self.prev_width = prev_width
+        self.prev_height = prev_height
 
     def get_current_background(self): # Returns the current background
         return self.background_list[self.current_index]
+    
+    def get_boundary_x(self): # Returns the x coordinate of the boundary
+        return self.boundary_x
+    
+    def get_boundary_y(self): # Returns the y coordinate of the boundary
+        return self.boundary_y
+    
+    def get_boundary_width(self): # Returns the width of the boundary
+        return self.boundary_width
+    
+    def get_boundary_height(self): # Returns the height of the boundary
+        return self.boundary_height
+    
+    def get_next_x(self):
+        return self.next_x
+    
+    def get_next_y(self):
+        return self.next_y
+    
+    def get_next_width(self):
+        return self.next_width
+    
+    def get_next_height(self):
+        return self.next_height
+    
+    def get_prev_x(self):
+        return self.prev_x
+    
+    def get_prev_y(self):
+        return self.prev_y
+    
+    def get_prev_width(self):
+        return self.prev_width
+    
+    def get_prev_height(self):
+        return self.prev_height
+    
--- connor_testing.py
+++ connor_testing.py
@@ -1,5 +1,5 @@
 # Main file where pygame game loop will exist.
-# We can use this file to declare and modify 
+# We can use this file to declare and modify
 # global variables and create objects and test
 # our code.
 
@@ -7,80 +7,101 @@ import pygame
 import sys
 from Background import Background
 
-
 pygame.init()
 
 screen = pygame.display.set_mode((1280, 1024))
 pygame.display.set_caption('Background Test')
-hell_background = Background("assets/hell_background.png", 1, 0, 0, 1.0, 0, 0, 0, 0)
-forest_path_background = Background("assets/forest_path_background.png", 1, 0, 0, 1.0, 0, 0, 0, 0)
-background2 = Background("assets/background2.png", 1, 0, 0, 1.0, 0, 0, 0, 0)
+
+hell_background = Background("assets/hell_background.png", 1, 0, 0, 1.0, 100, 100, 1080, 820, 300, 300, 100, 100)
+forest_path_background = Background("assets/forest_path_background.png", 1, 0, 0, 1.0, 100, 100, 1080, 820, 300, 300, 100, 100)
+background2 = Background("assets/background2.png", 1, 0, 0, 1.0, 100, 100, 1080, 820, 300, 300, 100 ,100)
+
 backgrounds = [forest_path_background, hell_background, background2]
 background_index = 0
 
 clock = pygame.time.Clock()
 
-#Circle properties
+# Circle properties
 circle_x, circle_y = 200, 200
 circle_radius = 20
 circle_speed = 20
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
-
-#Boundary properties
-# Boundary properties
-BOUNDARY_X, BOUNDARY_Y = 100, 100
-BOUNDARY_WIDTH, BOUNDARY_HEIGHT = 500, 500
 BLACK = (0, 0, 0)
 
 
+# Flag to prevent continuous background switching
+next_triggered = False
+prev_triggered = False
+
 # Game loop
-while True:
+running = True
+while running:
+    clock.tick(60)
     screen.fill((0, 0, 0))  # Fill the screen with black
-    
+
     # Display the current background
-    screen.blit(backgrounds[background_index].background_list[0], (background2.x, background2.y))
+    screen.blit(backgrounds[background_index].background_list[0], (backgrounds[background_index].x, backgrounds[background_index].y))
 
     # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        keys = pygame.key.get_pressed()
-        new_x, new_y = circle_x, circle_y
-        if keys[pygame.K_a]:
-            new_x -= circle_speed
-        if keys[pygame.K_d]:
-            new_x += circle_speed
-        if keys[pygame.K_w]:
-            new_y -= circle_speed
-        if keys[pygame.K_s]:
-            new_y += circle_speed
+            running = False
 
         # Handle keypress for moving between backgrounds
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:  # Move to the next background
-                if (background_index < len(backgrounds) - 1):
+                if background_index < len(backgrounds) - 1:
                     background_index += 1
-                    backgrounds[background_index]
             elif event.key == pygame.K_LEFT:  # Move to the previous background
-                if (background_index > 0):
+                if background_index > 0:
                     background_index -= 1
-                    backgrounds[background_index]
-            elif event.key == pygame.K_UP:
-                background_index = 1
-            elif event.key == pygame.K_DOWN:
-                background_index = 0
-            
-            if (BOUNDARY_X + circle_radius <= new_x <= BOUNDARY_X + BOUNDARY_WIDTH - circle_radius and
-            BOUNDARY_Y + circle_radius <= new_y <= BOUNDARY_Y + BOUNDARY_HEIGHT - circle_radius):
-                circle_x, circle_y = new_x, new_y
+
+    # Movement logic (should be outside event loop for continuous movement)
+    keys = pygame.key.get_pressed()
+    new_x, new_y = circle_x, circle_y
+    if keys[pygame.K_a]:
+        new_x -= circle_speed
+    if keys[pygame.K_d]:
+        new_x += circle_speed
+    if keys[pygame.K_w]:
+        new_y -= circle_speed
+    if keys[pygame.K_s]:
+        new_y += circle_speed
+
+    # Check if the circle is inside the boundary before updating position
+    if (backgrounds[background_index].get_boundary_x() + circle_radius <= new_x <= backgrounds[background_index].get_boundary_x() + backgrounds[background_index].get_boundary_width() - circle_radius and
+            backgrounds[background_index].get_boundary_y() + circle_radius <= new_y <= backgrounds[background_index].get_boundary_y() + backgrounds[background_index].get_boundary_height() - circle_radius):
+        circle_x, circle_y = new_x, new_y
+
+    # Check if the circle is inside the next zone
+    if (backgrounds[background_index].get_next_x() <= circle_x <= backgrounds[background_index].get_next_x() + backgrounds[background_index].get_next_width() and
+            backgrounds[background_index].get_next_y() <= circle_y <= backgrounds[background_index].get_next_y() + backgrounds[background_index].get_next_height()):
+        if not next_triggered:  # Only change background if not already triggered
+            if background_index < len(backgrounds) - 1:
+                background_index += 1
+            next_triggered = True
+    else:
+        next_triggered = False  # Reset flag when leaving the next box
 
 
-    pygame.draw.rect(screen, BLACK, (BOUNDARY_X, BOUNDARY_Y, BOUNDARY_WIDTH, BOUNDARY_HEIGHT), 2)  # Draw boundary
+# Check if the circle is inside the prev zone
+    if (backgrounds[background_index].get_prev_x() <= circle_x <= backgrounds[background_index].get_prev_x() + backgrounds[background_index].get_prev_width() and
+            backgrounds[background_index].get_prev_y() <= circle_y <= backgrounds[background_index].get_prev_y() + backgrounds[background_index].get_prev_height()):
+        if not prev_triggered:  # Only change background if not already triggered
+            if background_index > 0:
+                background_index -= 1
+            prev_triggered = True
+    else:
+        prev_triggered = False  # Reset flag when leaving the prev box
+
+
+    # Draw boundary, trigger box, and circle
+    pygame.draw.rect(screen, BLACK, (backgrounds[background_index].get_boundary_x(), backgrounds[background_index].get_boundary_y(), backgrounds[background_index].get_boundary_width(), backgrounds[background_index].get_boundary_height()), 2)  # Draw boundary
+    pygame.draw.rect(screen, BLACK, (backgrounds[background_index].get_next_x(), backgrounds[background_index].get_next_y(), backgrounds[background_index].get_next_width(), backgrounds[background_index].get_next_height()), 2)  # Draw trigger box
     pygame.draw.circle(screen, BLUE, (circle_x, circle_y), circle_radius)
-    pygame.display.update()  # Update the display
-    clock.tick(60)  # Limit the frame rate to 60 FPS
-    
 
+    pygame.display.update()  # Update the display
+
+pygame.quit()
+sys.exit()
