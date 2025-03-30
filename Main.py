734a53903faa8d13e8de860e0065a6cec4e34c86
mr-chris-reed <chrree@bhasd.org>
2025-03-30 19:54:11 -0400

    midway through changes

--- Main.py
+++ Main.py
@@ -36,20 +36,18 @@ CANVAS = pygame.display.set_mode((0, 0), FULLSCREEN)
 
 # object creation
 start_screen = Start_Screen("assets/start_screen.png", leaderboard, 1, 0, 0, HEIGHT, WIDTH)
-hell_background = Background("assets/hell_background.png", 1, 0, 0, 1.0, 100, 100, 1080, 820, 300, 300, 100, 100, 100, 100, 100, 100, False, False)
-forest_path_background = Background("assets/forest_path_background.png", 1, 0, 0, 1.0, 100, 100, 1080, 820, 300, 300, 100, 100, 100, 100, 100, 100, False, False)
-castle_background = Background("assets/castle_background.png", 1, 0, 0, 1.0, 100, 100, 1080, 820, 300, 300, 100 ,100, 100, 100, 100, 100, False, False)
+forest_path = Background("assets/forest_path_background.png", 1, 1, 0, 0, 50, WIDTH - 50, 50, HEIGHT - 50)
 capybarda = Player(
     200, 200,
     "assets/CapybardaRun_back.png", "assets/CapybardaRun_front.png", "assets/CapybardaRun_Side2.png", "assets/CapybardaRun_side.png", "assets/CapybardaIdle_front.png", "assets/CapybardaIdle_back.png",
     "assets/CapybardaIdle_back.png", "assets/CapybardaIdle_front.png", "assets/CapybardaIdle_side2.png", "assets/CapybardaIdle_side.png",
     6, 4, 4, 6, 4, 4, 4, 4, 4, 4,
-    0.5, 0.5, 0.5, 0.5, 0.5,
+    0.75, 0.75, 0.75, 0.75, 0.75,
     10, 10
 )
 
-backgrounds = [forest_path_background, hell_background, castle_background]
-current_background = forest_path_background
+backgrounds = [forest_path]
+current_background = start_screen
 
 # main game loop
 while running:
@@ -65,32 +63,16 @@ while running:
     if joysticks[0].get_button(11):
         game_start = True
     
-    if game_start == True:
-
-        if (joysticks[0].get_axis(0) > 0.5):
+    if not(game_start):
+        current_background = start_screen
+        CANVAS.blit(current_background.generate_return_surface(counter), (0, 0))
+    elif game_start:
+        current_background = backgrounds[Background.background_index]
+        CANVAS.blit(current_background.generate_return_surface(), (0, 0))
+        if joysticks[0].get_axis(0) > 0.50:
+            print(joysticks[0].get_axis(0))
             capybarda.up(counter)
-        if (joysticks[0].get_axis(0) < -0.5):
-            capybarda.down(counter)
-        if (joysticks[0].get_axis(1) > 0.5):
-            capybarda.right(counter)
-        if (joysticks[0].get_axis(1) < -0.5):
-            capybarda.left(counter)
-
-        Background.change_next_flag(backgrounds, capybarda.x_coord, capybarda.y_coord)
-        Background.change_prev_flag(backgrounds, capybarda.x_coord, capybarda.y_coord)
-
-        print(str(Background.background_index))
-        CANVAS.blit(backgrounds[Background.background_index].generate_background_surface(backgrounds, WIDTH, HEIGHT), (0, 0))
-        CANVAS.blit(capybarda.last_sprite, (capybarda.x_coord, capybarda.y_coord))
-
-
-    #if game_start == True:
-        
-        ### implement blitting of the Background.py class here
-    
-    if game_start == False:
-        current_background = start_screen.generate_return_surface(counter)
-        CANVAS.blit(current_background, (0, 0))
+        CANVAS.blit(capybarda.last_idle_sprite, (400, 400))
 
     if counter >= 600:
         counter = 0
