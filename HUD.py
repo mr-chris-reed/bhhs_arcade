41a4f87fbf09ba8b6b1841d40d7cd014cba604cb
25cchappell <25cchappell@students.bhasd.org>
2025-04-03 13:10:53 -0400

    Added the HUD.py file.

---
+++ HUD.py
@@ -0,0 +1,22 @@
+import pygame
+from Asset_Reader import Asset_Reader
+#from Player import Player (?)
+
+screen = pygame.display.set_mode((1280, 1024))
+
+class Start_Screen:
+
+    def __init__(self, player, timer, gold, health):
+        pass
+        #self.player =
+        #self.timer =
+        #self.gold =
+        #self.health =
+
+    
+    def update_timer():
+        pass
+    
+
+    def draw_HUD():
+        pass
