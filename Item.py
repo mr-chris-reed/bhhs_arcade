5ec67602ad2df45019e714c448565bd6edfc752f
CJames18 <25cjames@students.bhasd.org>
2025-04-07 13:14:15 -0400

    work on items

--- Item.py
+++ Item.py
@@ -7,7 +7,13 @@ from Asset_Reader import Asset_Reader
 
 class Item:
 
-    def __init__(self, item_image, item_name, interact):
+    def __init__(self, item_image, item_name, x, y, price):
         self.item_image = item_image
         self.item_name = item_name
-        self.interact = interact
+        self.interact = False
+        self.price = price
+        self.interact_surface = pygame.Surface((30, 30), pygame.SRCALPHA)
+        self.interact_rect = pygame.Rect(x, y, 30, 30)
+
+    def interact_check(self, Player):
+        pass
