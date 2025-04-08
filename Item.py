98796d8045258ff6831a3f3142ee64f876905976
CJames18 <25cjames@students.bhasd.org>
2025-04-08 13:13:46 -0400

    interact and draw function

--- Item.py
+++ Item.py
@@ -8,12 +8,24 @@ from Asset_Reader import Asset_Reader
 class Item:
 
     def __init__(self, item_image, item_name, x, y, price):
-        self.item_image = item_image
+        self.item_image = Asset_Reader(item_image, 1, 1.0).get_asset_list()[0]
         self.item_name = item_name
         self.interact = False
+        self.x = x
+        self.y = y
         self.price = price
-        self.interact_surface = pygame.Surface((30, 30), pygame.SRCALPHA)
-        self.interact_rect = pygame.Rect(x, y, 30, 30)
+        self.interact_surface = pygame.Surface((70, 70), pygame.SRCALPHA)
+        self.interact_rect = pygame.Rect(x, y, 70, 70)
+        self.picked_up = False
 
-    def interact_check(self, Player):
-        pass
+    def draw_item(self, CANVAS):
+        if (self.picked_up == False):
+            CANVAS.blit(self.item_image, (self.x, self.y))
+
+    def interact_check(self, player):
+        player_rect = pygame.Rect(player.x_coord, player.y_coord, player.width, player.height)
+        if self.interact_rect.colliderect(player_rect):
+            self.picked_up = True
+            return True
+        else:
+            return False
---
+++ item_testing.py
@@ -0,0 +1,137 @@
+# Main file where pygame game loop will exist.
+# We can use this file to declare and modify 
+# global variables and create objects and test
+# our code.
+
+import pygame
+from pygame.locals import *
+from Asset_Reader import Asset_Reader
+# from End_Screen import End_Screen
+from Start_Screen import Start_Screen
+from Background import Background
+from Player import Player
+from Projectile import Projectile
+from Item import Item
+
+# initialize pygame and pygame joystick
+pygame.init()
+pygame.joystick.init()
+
+# creating the clock
+clock = pygame.time.Clock()
+
+# constants
+WIDTH = 1280
+HEIGHT = 1024
+FPS = 30
+
+# global variables
+current_background = None
+previous_background_index = 0
+running = True
+game_start = False
+joysticks = []
+counter = 0
+previous_counter = 0
+leaderboard = [['CMC', "7.5"], ['CWJ', "7.8"], ['TGP', "8.1"]]
+notes_left = []
+notes_right = []
+notes_up = []
+notes_down= []
+
+# load sounds
+forest_sound = pygame.mixer.Sound("sounds/Forest_Scene_Concept.mp3")
+forest_sound.set_volume(0.20)
+castle_sound = pygame.mixer.Sound("sounds/Castle_Scene_Concept.mp3")
+castle_sound.set_volume(0.20)
+hell_sound = pygame.mixer.Sound("sounds/Boss_Intro_Concept.mp3")
+hell_sound.set_volume(0.20)
+
+# load items
+item_1 = Item("assets/item2.png", "Evil Trinket of Mischief and Deception", 500, 500, 1)
+
+CANVAS = pygame.display.set_mode((0, 0), FULLSCREEN)
+
+# object creation
+start_screen = Start_Screen("assets/start_screen.png", leaderboard, 1, 0, 0, HEIGHT, WIDTH)
+forest_path = Background("assets/forest_path_background.png", 1, 1, 0, 0, -150, WIDTH + 150, 50, HEIGHT - 50)
+castle = Background("assets/Castle.png", 1, 1, 0, 0, -150, WIDTH + 150, 50, HEIGHT - 50)
+hell = Background("assets/_Hell_.png", 1, 1, 0, 0, -150, WIDTH + 150, 50, HEIGHT - 50)
+capybarda = Player(
+    200, 200,
+    "assets/CapybardaRun_back.png", "assets/CapybardaRun_front.png", "assets/CapybardaRun_Side2.png", "assets/CapybardaRun_side.png", "assets/CapybardaIdle_front.png", "assets/CapybardaIdle_back.png",
+    "assets/CapybardaIdle_back.png", "assets/CapybardaIdle_front.png", "assets/CapybardaIdle_side2.png", "assets/CapybardaIdle_side.png",
+    6, 4, 4, 6, 4, 4, 4, 4, 4, 4,
+    0.6, 0.6, 0.6, 0.6, 0.6,
+    10, 10
+)
+# initial position of capybarda
+capybarda.x_coord = 100
+capybarda.y_coord = HEIGHT // 2
+
+backgrounds = [forest_path, castle, hell]
+current_background = start_screen
+
+# main game loop
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT: 
+            running = False
+
+    keys = pygame.key.get_pressed()
+    game_start = True
+    
+    if not(game_start):
+        current_background = start_screen
+        CANVAS.blit(current_background.generate_return_surface(counter), (0, 0))
+    elif game_start:
+        current_background = backgrounds[Background.background_index]
+        CANVAS.blit(current_background.generate_return_surface(), (0, 0))
+        if keys[pygame.K_w]:
+            if (current_background.check_can_move_up(capybarda)):
+                capybarda.up(counter)
+        elif keys[pygame.K_s]:
+            if (current_background.check_can_move_down(capybarda)):
+                capybarda.down(counter)
+        elif keys[pygame.K_d]:
+            if (current_background.check_can_move_right(capybarda)):
+                capybarda.right(counter)
+        if keys[pygame.K_a]:
+            if (current_background.check_can_move_left(capybarda)):
+                capybarda.left(counter)
+        if (current_background.check_if_in_next_box(capybarda) and Background.background_index < 2):
+            Background.background_index += 1
+            capybarda.x_coord = -100
+        if (current_background.check_if_in_prev_box(capybarda)):
+            if Background.background_index > 0:
+                Background.background_index -= 1
+            if Background.background_index == 0:
+                game_start = False
+            capybarda.x_coord = 1200
+
+        CANVAS.blit(capybarda.last_sprite, (capybarda.x_coord, capybarda.y_coord))
+        item_1.draw_item(CANVAS)
+
+    # play sounds
+    if game_start:
+        if Background.background_index == 0:
+            forest_sound.play()
+            castle_sound.stop()
+            hell_sound.stop()
+        elif Background.background_index == 1:
+            castle_sound.play()
+            forest_sound.stop()
+            hell_sound.stop()
+        elif Background.background_index == 2:
+            hell_sound.play()
+            forest_sound.stop()
+            castle_sound.stop()
+
+    if counter >= 600:
+        counter = 0
+        previous_counter = 0
+    else:
+        counter += 1
+
+    pygame.display.flip()
+    clock.tick(FPS)
