f559f70db6258dd354e8d2c82ac6ebb51c886854
TylerPham2 <25tpham@students.bhasd.org>
2025-04-23 13:11:45 -0400

    clock work after hitting left arrow key

--- tpham_test2.py
+++ tpham_test2.py
@@ -1,48 +1,55 @@
 import pygame
 
-# Define some colors
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 pygame.init()
 
-# Set the height and width of the screen
 size = [700, 500]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Timer Example")
 
-# Set up font and clock
+
 font = pygame.font.Font(None, 25)
 clock = pygame.time.Clock()
-
+scorelist = []
 # Variables
 frame_count = 0
 frame_rate = 60
 #main loopps
 running = True
+start = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and start == False:
+                start = True
+            elif event.key == pygame.K_LEFT and start == True:
+                start = False
 
     
     screen.fill(WHITE)
 
-    total_seconds = frame_count // frame_rate  # Total seconds passed
-    output_string = f"Time: {total_seconds}"
-
+    total_seconds = frame_count // frame_rate  # total seconds passed
+    score= f"Time: {total_seconds}"
+    
     # render _ display
-    text = font.render(output_string, True, BLACK)
-    screen.blit(text, [250, 250])
+    text = font.render(score, True, BLACK)
+    if start:
+        screen.blit(text, [250, 250])
 
-    # Update frame count and display
-    frame_count += 1
-    pygame.display.flip()
+       
+        frame_count += 1
+        pygame.display.flip()
 
-    # Limit frames per second
-    clock.tick(frame_rate)
-
-# Close pygame properly
+        
+        clock.tick(frame_rate)
+    #elif not start and
+     #   print(score)
+#
 pygame.quit()
 
 #reminder of me, make it work so only after a button is pressed the timer starts and after another is pressed it resets
