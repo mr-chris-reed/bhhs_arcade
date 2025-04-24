a7cb3295c91e8e6db6ce05d3d5167162970635e6
TylerPham2 <25tpham@students.bhasd.org>
2025-04-24 12:44:18 -0400

    timer works

--- tpham_test2.py
+++ tpham_test2.py
@@ -8,7 +8,7 @@ pygame.init()
 
 size = [700, 500]
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Timer Example")
+
 
 
 font = pygame.font.Font(None, 25)
@@ -24,32 +24,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and start == False:
-                start = True
-            elif event.key == pygame.K_LEFT and start == True:
-                start = False
-
-    
     screen.fill(WHITE)
-
     total_seconds = frame_count // frame_rate  # total seconds passed
     score= f"Time: {total_seconds}"
+    text = font.render(score, True, BLACK) # render _ display
     
-    # render _ display
-    text = font.render(score, True, BLACK)
-    if start:
-        screen.blit(text, [250, 250])
-
-       
-        frame_count += 1
-        pygame.display.flip()
-
-        
-        clock.tick(frame_rate)
-    #elif not start and
-     #   print(score)
-#
+    screen.blit(text, [250, 250])
+    frame_count += 1
+    pygame.display.flip()
+    clock.tick(frame_rate)
+   
 pygame.quit()
 
-#reminder of me, make it work so only after a button is pressed the timer starts and after another is pressed it resets
