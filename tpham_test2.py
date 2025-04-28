fea498d88143d578e4d2b076365bf51ffcce227b
TylerPham2 <25tpham@students.bhasd.org>
2025-04-28 13:13:12 -0400

    addded fractions of seconds to hundreths placeA

--- tpham_test2.py
+++ tpham_test2.py
@@ -14,25 +14,29 @@ screen = pygame.display.set_mode(size)
 font = pygame.font.Font(None, 25)
 clock = pygame.time.Clock()
 scorelist = []
+
+
+
+
 # Variables
 frame_count = 0
 frame_rate = 60
 #main loopps
 running = True
 start = False
+#clock stuff
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill(WHITE)
-    total_seconds = frame_count // frame_rate  # total seconds passed
-    score= f"Time: {total_seconds}"
+    total_seconds = frame_count / frame_rate  # total seconds passed
+    roundedtime=round(total_seconds,2)# rounds time to 2 decimal places
+    score= f"Time: {roundedtime}"
     text = font.render(score, True, BLACK) # render _ display
-    
     screen.blit(text, [250, 250])
     frame_count += 1
     pygame.display.flip()
     clock.tick(frame_rate)
-    
 pygame.quit()
 
