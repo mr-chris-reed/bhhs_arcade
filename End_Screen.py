eb9d77baede23403e8ea998f72c702623df2bd07
CJames18 <144144195+CJames18@users.noreply.github.com>
2025-05-01 13:12:16 -0400

    changes to make leaderboard work

--- End_Screen.py
+++ End_Screen.py
@@ -10,6 +10,9 @@ class End_Screen:
     # would want it to hold position, player's initials, and time to complete.
     # We would also want a message to tell players how to get back to the start.
     ###
+
+    Leaderboard = Leaderboard()
+    updated_leaderboard = ''
     
     def __init__(
             self,
@@ -111,6 +114,8 @@ class End_Screen:
                 if self.button_move > 0.5 and self.pressedVisiblity==False:
                     self.pressedVisiblity = True            
                     self.inputVisible = False
+                    name = self.name
+                    Leaderboard.add_to_leaderboard(self.Leaderboard, name, 20)
                     ###
                     self.name = ''
                     ###
